f9b1f97811d0d4ce8c9d23cb4ba2256c3371b551
buhleqampi <buhle3857@gmail.com>
2022-12-14 09:32:25 +0200

    second

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,50 +1,69 @@
 <template>
-    <li v-for="value in myObject">
-    {{ value.name }}
-    </li>
+    <div class="col-2" v-for="item in myObject">
+        <div>
+        <div class="card">
+            <img :src="item.image" 
+            style="width: 30rem;margin-left:3rem" 
+            />
+            <div class="card-body">
+                <h5 class="card-title">{{ item.product }}</h5>
+                <p class="card-text">{{ item.brand }}</p>
+                <p class="price">{{ item.price }}</p>
+                <button class="btn btn-primary">Add to checkout</button>
+            </div>
+        </div>
+    </div>
+    </div>
 </template>
 
 <script>
 export default {
-data() {
-return {
-    myObject: [
-        {
-        name: 'Banana',
-        description: 'Description of the product',
-        price: 1,
-        quantity: 1,
-        image: 'https://www.primefaces.org/wp-content/uploads/2020'
-    },
-        {
-        name: 'Strawberries',
-            description: 'Description of the product',
-            price: 2,
-            quantity: 2,
-            image: 'https://www.primefaces.org/wp-content/uploads/2020'
-        },
-        {
-            name: 'Melons',
-            description: 'Description of the product',
-            price: 3,
-            quantity: 3,
-            image: 'https://www.primefaces.org/wp-content/uploads/2020'
-        },
-        {
-            name: 'Kiwi',
-            description: 'Description of the product',
-            price: 4,
-            quantity: 4,
-            image: 'https://www.primefaces.org/wp-content/uploads/2020'
-        
-        },
-    ],
-};
-}
+    data() {
+        return {
+            myObject: [
+                {
+                    "id": 1,
+                    "product": "Foundations",
+                    "brand": "Fentybeauty",
+                    "image": "https://i.postimg.cc/T3fvVqJ8/foundationsjpg-image-668x550.jpg",
+                    "price": "R600,00"
+
+                },
+                {
+                    "id": 2,
+                    "product": "Concealers",
+                    "brand": "Mac",
+                    "image": "https://i.postimg.cc/prJqR8h8/IMG-0181.webp",
+                    "price": "R400,00"
+                },
+                {
+                    "id": 3,
+                    "product": "Bronzers",
+                    "brand": "Yardley",
+                    "image": "https://i.postimg.cc/k578PZNj/YAR-F122282-BLANK-1.jpg",
+                    "price": "R250,00"
+                },
+                {
+                    "id": 4,
+                    "product": "Highlighters",
+                    "brand": "Rimmel",
+                    "image": "https://i.postimg.cc/tJnpcqYL/THREE.jpg",
+                    "price": "R180,00"
+                },
+            ],
+        };
+    }
 }
 
 </script>
 
-<style scoped>
+<!-- <style scoped>
+.container {
+    display:grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+    margin-bottom:5px;
 
-</style>
+}
+
+</style> -->
